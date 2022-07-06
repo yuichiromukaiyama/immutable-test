9ba8b5fc288d394426166f5b218da0743e04e977
yuichiro mukaiyama <yuichiro.mukaiyama@g.softbank.co.jp>
2022-07-06 19:08:39 +0900

    [add] First commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,61 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from "react";
+import { Map } from "immutable";
+
+type IForm = { a: string; b: string; c: string };
 
 function App() {
+  const [state, setState] = React.useState(Map({ a: "", b: "", c: "" }));
+
+  const handle = (key: keyof IForm, value: string) => {
+    setState(state.set(key, value));
+  };
+
+  useEffect(() => {
+    console.log("入力を監視");
+    console.log(state.toJSON());
+  }, [state]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      style={{
+        width: "100vw",
+        height: "95vh",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <div
+        style={{
+          borderRadius: "10px",
+          border: "1px solid black",
+          padding: "20px",
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "stretch",
+          alignItems: "stretch",
+          gap: "15px",
+          width: "80vw",
+          maxWidth: "500px",
+        }}
+      >
+        <p style={{ fontSize: "1em", fontWeight: "bold" }}>入力フォーム</p>
+        <input
+          value={state.get("a")}
+          onChange={(e) => handle("a", e.currentTarget.value)}
+          placeholder="入力A"
+        />
+        <input
+          value={state.get("b")}
+          onChange={(e) => handle("b", e.currentTarget.value)}
+          placeholder="入力B"
+        />
+        <input
+          value={state.get("C")}
+          onChange={(e) => handle("c", e.currentTarget.value)}
+          placeholder="入力C"
+        />
+      </div>
     </div>
   );
 }
